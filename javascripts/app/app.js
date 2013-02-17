ff127b323a75ffd442b36e50ac6e45aa810a3551
Nick Bury <nmbury@gmail.com>
2013-02-17 15:54:19 -0500

    displays tweets by prepending <p> elements then removes them after 5 seconds

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,5 +1,13 @@
 var main = function () {
-	$("#tweets").html("Hello World!").fadeOut(5000);
+	var twitter = new ctwitter.CTwitter();
+	twitter.stream("statuses/filter", {lang:"en", track:["phonegap"]}, function(stream) {
+		stream.on("data", function(tweet) {
+			$("#tweets").prepend("<p class='response'>" + tweet.text + "</p>");
+			$(".response").fadeOut(5000, function() {
+				$(this).remove();
+			});
+		});
+	});
 }
 
 
