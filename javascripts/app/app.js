de34a54ea6e15b2e432845347446c7d6538f1e5f
Nick Bury <nmbury@gmail.com>
2013-02-25 16:01:43 -0500

    Change to 4 space indent. Make JSLint happier.

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,14 +1,15 @@
 var main = function () {
+    "use strict";
 	var twitter = new ctwitter.CTwitter();
-	twitter.stream("statuses/filter", {lang:"en", track:["phonegap"]}, function(stream) {
-		stream.on("data", function(tweet) {
+	twitter.stream("statuses/filter", {lang: "en", track: ["phonegap"]}, function (stream) {
+		stream.on("data", function (tweet) {
 			$("#tweets").prepend("<p class='response'>" + tweet.text + "</p>");
-			$(".response").fadeOut(5000, function() {
+			$(".response").fadeOut(5000, function () {
 				$(this).remove();
 			});
 		});
 	});
-}
+};
 
 
 $(document).ready(main);
